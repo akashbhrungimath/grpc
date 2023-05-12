6c20376e5c31f8618ae09869c1c5dfe51a1fd973
Akash <94945180+akashbhrungimath@users.noreply.github.com>
2023-05-12 14:04:43 +0530

    BidirectionalStreaming

--- Program.cs
+++ Program.cs
@@ -1,8 +1,4 @@
-using GrpcClientCertificateServer.Services;
-using Microsoft.AspNetCore.Authentication.Certificate;
-using Microsoft.AspNetCore.Server.Kestrel.Core;
-using Microsoft.AspNetCore.Server.Kestrel.Https;
-using System.Security.Claims;
+using BidirectionalStreamingServer2.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,40 +6,12 @@ var builder = WebApplication.CreateBuilder(args);
 // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
 
 // Add services to the container.
-/*public static IHostBuilder CreateHostBuilder(string[] args) =>
-    Host.CreateDefaultBuilder(args)
-    .ConfigureWebHostDefaults(webBuilder =>
-    {
-        webBuilder.UseStartup<Program>();
-        webBuilder.ConfigureKestrel(o =>
-        {
-            o.ConfigureHttpsDefaults(o =>
-            {
-                o.ClientCertificateMode = ClientCertificateMode.RequireCertificate;
-                o.ServerCertificate = < MyCertificateWithPrivateKey >;
-            });
-        });
-    });*/
 builder.Services.AddGrpc();
-builder.Services.AddAuthentication(x =>
-{
-    x.DefaultAuthenticateScheme = CertificateAuthenticationDefaults.AuthenticationScheme;
-    x.DefaultChallengeScheme = CertificateAuthenticationDefaults.AuthenticationScheme;
-}).AddCertificate();
-builder.Services.Configure<KestrelServerOptions>(options =>
-{
-    options.ConfigureHttpsDefaults(options =>
-    {
-        options.ClientCertificateMode = ClientCertificateMode.RequireCertificate;
-    });
-});
-builder.Services.AddAuthorization();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.UseAuthentication();
-app.UseAuthorization();
-app.MapGrpcService<StudentService>();
+app.MapGrpcService<StreamingService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
---
+++ StreamingService.cs
@@ -0,0 +1,30 @@
+﻿using Grpc.Core;
+using System.Reflection.Emit;
+
+namespace BidirectionalStreamingServer2.Services
+{
+    public class StreamingService : StartStreaming.StartStreamingBase
+    {
+        private readonly ILogger <StreamingService> _logger;
+
+        public StreamingService(ILogger <StreamingService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override async Task FindSum(IAsyncStreamReader<Inputs> requestStream, IServerStreamWriter<Outputs> responseStream, ServerCallContext context)
+        {
+            var output = new Outputs { Sum = 0 };
+            while (await requestStream.MoveNext())
+            {
+                var input = requestStream.Current;
+                output.Sum += input.Num;
+            }
+            while (!context.CancellationToken.IsCancellationRequested)
+            {
+                await responseStream.WriteAsync(output);
+                await Task.Delay(TimeSpan.FromSeconds(1));
+            }
+        }
+    }
+}
